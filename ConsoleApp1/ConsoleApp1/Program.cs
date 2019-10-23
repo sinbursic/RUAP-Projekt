5b03bbec868ac46359ad02216193fc30ef6d456b
SBursic <sinbursic@etfos.hr>
2019-10-23 17:53:47 +0200

    cherry-pick1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Pozdrav iz aplikacije od Siniše Buršića");
-            Console.WriteLine("Broj indeksa: 3005000");
+
         }
     }
 }
